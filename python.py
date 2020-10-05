3f7d24669301cf38346177d741d296ae92bb4053
CarlosCamelo <carloscamelo100@gmail.com>
2020-10-05 06:46:48 -0600

    archivo de python incial

---
+++ python.py
@@ -0,0 +1,12 @@
+"""Esto es el principio del curso de python
+nombre = input('Cual es tu nombre: ')
+print('Tu nombre es: ',nombre)
+numero = input('escribe un numero: ')
+numero = int(input('escribe un numero: '))
+print(type(numero))"""
+
+saludo = ('saludo')
+nombre = input('Cual es tu nombre: ')
+total = saludo + nombre
+print(saludo,' ',nombre)
+print('la longitud es: ',len(total))
